fa33f382ece7756879a2247d470f60dc3cbad30b
lornebb <51881266+lornebb@users.noreply.github.com>
2020-10-02 22:02:34 +0200

    init commit - basic python input tests and vs code installs

---
+++ python.py
@@ -0,0 +1,8 @@
+msg = "hello world!"
+print(msg)
+
+username = input("enter your username here ")
+team_name = input("type your team name here ")
+star_player = input("type your star player here ")
+manager_name = input("type your favourite manager name here ")
+print("hello" + username + ", we hope you kick some ass with " + team_name + " and "+ star_player + " with " + manager_name + " at the helm.")
