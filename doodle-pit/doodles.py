0d56c2f53504050c890b7ff42c4b8caca9d50f2d
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2020-10-09 21:40:39 +0200

    Added selections using PyInquirer to select home and away team

---
+++ doodle-pit/doodles.py
@@ -0,0 +1,47 @@
+from __future__ import print_function, unicode_literals
+from PyInquirer import prompt, print_json, Separator
+from pprint import pprint
+
+
+def get_delivery_options(answers):
+    options = ['bike', 'car', 'truck']
+    if answers['size'] == 'jumbo':
+        options.append('helicopter')
+    return options
+
+
+questions = [
+    {
+        'type': 'list',
+        'name': 'theme',
+        'message': 'What do you want to do?',
+        'choices': [
+            'Order a pizza',
+            'Make a reservation',
+            Separator(),
+            'Ask for opening hours',
+            {
+                'name': 'Contact support',
+                'disabled': 'Unavailable at this time'
+            },
+            'Talk to the receptionist'
+        ]
+    },
+    {
+        'type': 'list',
+        'name': 'size',
+        'message': 'What size do you need?',
+        'choices': ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+        'filter': lambda val: val.lower()
+    },
+    {
+        'type': 'list',
+        'name': 'delivery',
+        'message': 'Which vehicle you want to use for delivery?',
+        'choices': get_delivery_options,
+    },
+]
+
+answers = prompt(questions)
+print_json(answers)  # use the answers as input for your app
+pprint(answers)
---
+++ selections.py
@@ -0,0 +1,52 @@
+from __future__ import print_function, unicode_literals
+from PyInquirer import prompt, print_json, Separator
+from pprint import pprint
+
+teams = ["Arsenal", "Aston Villa", "Brighton", "Burnley",
+         "Chelsea", "Crystal Palace", "Everton", "Fulham",
+         "Leeds", "Leicester", "Liverpool", "Man City",
+         "Man Utd", "Newcastle", "Sheffield Utd", "Southampton",
+         "Spurs", "West Brom", "West Ham", "Wolves"]
+
+home_teams = ["Arsenal", "Aston Villa", "Brighton", "Burnley",
+              "Chelsea", "Crystal Palace", "Everton", "Fulham",
+              "Leeds", "Leicester"]
+
+away_teams = ["Liverpool", "Man City",
+              "Man Utd", "Newcastle", "Sheffield Utd", "Southampton",
+              "Spurs", "West Brom", "West Ham", "Wolves"]
+
+def get_team_options(answers):
+    options = teams
+    return options
+
+def get_away_team_options(answers):
+    options = away_teams
+    return options
+
+def get_home_team_options(answers):
+    options = home_teams
+    return options
+
+
+questions = [
+    {
+        'type': 'list',
+        'name': 'home_team_goals',
+        'message': "Choose a home team that you think will score the most goals",
+        'choices': get_home_team_options,
+
+    },
+
+    {
+        'type': 'list',
+        'name': 'away_team_goals',
+        'message': "Choose an away team that you think will score the most goals",
+        'choices': get_away_team_options,
+
+    },
+]
+
+answers = prompt(questions)
+print_json(answers)  # use the answers as input for your app
+pprint(answers)
